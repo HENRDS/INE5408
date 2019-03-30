93293ee2613324651cfc062615fdf268da5e7363
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2019-03-29 21:27:36 -0300

    add_obj fix

--- views/add_obj.py
+++ views/add_obj.py
@@ -4,16 +4,16 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gtk
+from geometry import hpt
+from shapes import Line
 
 
 class AddLineHandler(WinAddLine):
 
     def on_btn_add_line_clicked(self, sender: Gtk.Button) -> None:
-        line = self.app_handler.model.add_line(self.entry_add_line_name.get_text(),
-                                               float(self.entry_add_line_x.get_text()),
-                                               float(self.entry_add_line_y.get_text()),
-                                               float(self.entry_add_line_x2.get_text()),
-                                               float(self.entry_add_line_y2.get_text()))
-
-
-
+        line = Line(self.entry_add_line_name.get_text(),
+                    hpt(float(self.entry_add_line_x.get_text()),
+                        float(self.entry_add_line_y.get_text())),
+                    hpt(float(self.entry_add_line_x2.get_text()),
+                        float(self.entry_add_line_y2.get_text())))
+        self.app_handler.model.add_object(line)
